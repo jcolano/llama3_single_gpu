fec82d8f3a997a35b61d5490ee59138bbb0a21f6
jcolano <1131538+jcolano@users.noreply.github.com>
2024-07-23 14:31:30 -0400

    Add files via upload

--- llama31/model.py
+++ llama31/model.py
@@ -8,20 +8,14 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # This software may be used and distributed in accordance with the terms of the Llama 3 Community License Agreement.
 
-# Updated by Juan Olano 4/18/2024 
+# Updated by Juan Olano 7/23/2024 
 # Simplified the code to adapt it for a single GPU. (https://github.com/jcolano/llama3_single_gpu.git)
 
 import math
 from typing import Optional, Tuple
 
-import fairscale.nn.model_parallel.initialize as fs_init
 import torch
 import torch.nn.functional as F
-# from fairscale.nn.model_parallel.layers import (
-#     ColumnParallelLinear,
-#     RowParallelLinear,
-#     VocabParallelEmbedding,
-# )
 from torch import nn
 
 from args import ModelArgs
